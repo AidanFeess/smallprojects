10865d1235a181e2335f8570ad56423dd8c4cb5b
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-04-09 11:42:51 -0500

    game start

--- Class2/Class2 Game/Game.py
+++ Class2/Class2 Game/Game.py
@@ -1,19 +1,122 @@
 from DeathFunc import death
-######################################################################
-# the blueprint for a room
+from random import choice
+import random
+import numpy
+###################################################################### PLAYER VARIABLES
+Gold = 0
+Inventory = []
+
+MaxHealth = 100
+Health = 100
+
+Hunger = 10
+MaxHunger = 10
+
+MaxThirst = 10
+Thirst = 10
+# Amount of hunger/thirst lost per turn
+HungerLoss = -1
+ThirstLoss = -2
+###################################################################### CLASSES
+
+class Item:
+    def __init__(self, name : str, description : str, worth : int, grabbable : bool):
+        self.name = name
+        self.description = description
+        self.worth = worth
+        self.grabbable = grabbable
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def description(self):
+        return self._description
+    @description.setter
+    def description(self, value):
+        self._description = value
+
+    @property
+    def worth(self):
+        return self._worth
+    @worth.setter
+    def worth(self, value):
+        self._worth = value
+
+    @property
+    def grabbable(self):
+        return self._grabbable
+    @grabbable.setter
+    def grabbable(self, value):
+        self._grabbable = value
+
+    @property
+    def edible(self):
+        return self._edible
+    @edible.setter
+    def edible(self, value):
+        self._edible = value
+
+class Weapon(Item):
+    def __init__(self, name : str, description : str, worth : int, damage : int):
+        self.name = name
+        self.description = description
+        self.worth = worth
+        # Weapons are always grabbable
+        self.grabbable = True
+
+        self.damage = damage
+
+    def DealDamage(self, enemy):
+        enemy.TakeDamage(self.damage)
+
+class Food(Item):
+    def __init__(self, name : str, description : str, worth : int, healing : int, hunger : int, thirst : int):
+        self.name = name
+        self.description = description
+        self.worth = worth
+        # Food is always grabbable
+        self.grabbable = True
+        
+        self.healing = healing
+        self.hunger = hunger
+        self.thirst = thirst
+        
+    def Use(self):
+        Health = numpy.clip(Health + self.healing, 0, MaxHealth)
+        Hunger = numpy.clip(Hunger + self.hunger, 0, MaxHunger)
+        Thirst = numpy.clip(Thirst + self.thirst, 0, MaxThirst)
+        Inventory.remove(self)
+        
+class Character:
+    def __init__(self, name, health, resistances, level):
+        self.name = name
+        self.health = health
+        self.resistances = resistances
+        self.level = level
+        self.drops = {}
+
+    def TakeDamage(self, damage):
+        self.health -= damage
+
+    def DealDamage(self, enemy, damage):
+        enemy.TakeDamage(damage)
+
 class Room:
 # the constructor
-    def __init__(self, name):
+    def __init__(self, name : str, isExit : bool, Position : tuple):
         # rooms have a name, exits (e.g., south), exit locations
         # (e.g., to the south is room n), items (e.g., table), item
         # descriptions (for each item), and grabbables (things that
         # can be taken into inventory)
         self.name = name
-        self.exits = []
-        self.exitLocations = []
+        self.isExit = isExit
         self.items = []
-        self.itemDescriptions = []
-        self.grabbables = []
+        self.Position = Position
     
     # getters and setters for the instance variables
     @property
@@ -23,17 +126,11 @@ class Room:
     def name(self, value):
         self._name = value
     @property
-    def exits(self):
-        return self._exits
-    @exits.setter
-    def exits(self, value):
-        self._exits = value
-    @property
-    def exitLocations(self):
-        return self._exitLocations
-    @exitLocations.setter
-    def exitLocations(self, value):
-        self._exitLocations = value
+    def isExit(self):
+        return self._isExit
+    @isExit.setter
+    def isExit(self, value):
+        self._isExit = value
     @property
     def items(self):
         return self._items
@@ -47,37 +144,17 @@ class Room:
     def itemDescriptions(self, value):
         self._itemDescriptions = value
     @property
-    def grabbables(self):
-        return self._grabbables
-    @grabbables.setter
-    def grabbables(self, value):
-        self._grabbables = value
-    
-    # adds an exit to the room
-    # the exit is a string (e.g., north)
-    # the room is an instance of a room
-    def addExit(self, exit, room):
-    # append the exit and room to the appropriate lists
-        self._exits.append(exit)
-        self._exitLocations.append(room)
-    # adds an item to the room
-    # the item is a string (e.g., table)
-    # the desc is a string that describes the item (e.g., it is made
-    # of wood)
-    def addItem(self, item, desc):
-    # append the item and exit to the appropriate lists
+    def Position(self):
+        return self._Position
+    @Position.setter
+    def Position(self, value):
+        self._Position = value
+    # Adding and removing items
+    def addItem(self, item):
+        # Create an item object with given parameters
         self._items.append(item)
-        self._itemDescriptions.append(desc)
-    # adds a grabbable item to the room
-    # the item is a string (e.g., key)
-    def addGrabbable(self, item):
-    # append the item to the list
-        self._grabbables.append(item)
-    # removes a grabbable item from the room
-    # the item is a string (e.g., key)
-    def delGrabbable(self, item):
-    # remove the item from the list
-        self._grabbables.remove(item)
+    def delItem(self, item):
+        self._items.remove(item)
     # returns a string description of the room
     def __str__(self):
     # first, the room name
@@ -85,75 +162,56 @@ class Room:
     # next, the items in the room
         s += "You see: "
         for item in self.items:
-            s += item + " "
+            s += item.name + " "
             s += "\n"
-    # next, the exits from the room
-        s += "Exits: "
-        for exit in self.exits:
-            s += exit + " "
         return s
 
+# List of room flavor descriptors
+RoomDescriptors = ["grand", "great", "dark", "dim"]
+RoomCeilingHeight = ["low", "high", "normal"]
+RoomMaterialState = [" pristine", " desolate", "n average"]
 
-######################################################################
+# List of items
+OtherItems = [Item("Used 2006 Toyota Corolla", "A used 2006 model Toyota Corolla!", 0, False)]
+WeaponItems = [Weapon("debugsword", "A developer's debug sword!", 100, 10)]
+FoodItems = [Food("debugmeat", "A developer's debug meat!", 100, -25, 1, 1)]
+
+# List of enemies
+###################################################################### INTERACTION FUNCTIONS
 # creates the rooms
 def createRooms():
-    # r1 through r4 are the four rooms in the mansion
-    # currentRoom is the room the player is currently in (which can
-    # be one of r1 through r4)
-    # since it needs to be changed in the main part of the program,
-    # it must be global
+    # Randomly generate rooms
     global currentRoom
-    # create the rooms and give them meaningful names
-    r1 = Room("Room 1")
-    r2 = Room("Room 2")
-    r3 = Room("Room 3")
-    r4 = Room("Room 4")
-    # add exits to room 1
-    r1.addExit("east", r2) # -> to the east of room 1 is room 2
-    r1.addExit("south", r3)
-    # add grabbables to room 1
-    r1.addGrabbable("key")
-    # add items to room 1
-    r1.addItem("chair", "It is made of wicker and no one is sitting on it.")
-    r1.addItem("table", "It is made of oak. A golden key rests on it.")
-    # add exits to room 2
-    r2.addExit("west", r1)
-    r2.addExit("south", r4)
-    # add items to room 2
-    r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed.")
-    r2.addItem("fireplace", "It is full of ashes.")
-    # add exits to room 3
-    r3.addExit("north", r1)
-    r3.addExit("east", r4)
-    # add grabbables to room 3
-    r3.addGrabbable("book")
-    # add items to room 3
-    r3.addItem("bookshelves", "They are empty. Go figure.")
-    r3.addItem("statue", "There is nothing special about it.")
-    r3.addItem("desk", "The statue is resting on it. So is a book.")
-    # add exits to room 4
-    r4.addExit("north", r2)
-    r4.addExit("west", r3)
-    r4.addExit("south", None) # DEATH!
-    # add grabbables to room 4
-    r4.addGrabbable("6-pack")
-    11
-    # add items to room 4
-    r4.addItem("brew_rig", "Gourd is brewing some sort of oatmeal stout on the brew rig. A 6-pack is resting beside it.")
-    # set room 1 as the current room at the beginning of the game
-    currentRoom = r1
+    MapSize = (3, 3)
+    Rooms = []
+    # Create a room for each X and Y
+    for y in range(MapSize[1]):
+        Row = []
+        for x in range(MapSize[0]):
+            RoomName = "a "
+            RoomName += f"{choice(RoomDescriptors)} room with a {choice(RoomCeilingHeight)} ceiling. It appears to be in a{choice(RoomMaterialState)} condition"
+            NewRoom = Room(RoomName, False, (x, y))
+            # Add a random number of random foods
+            for _ in range(random.randint(0, 2)):
+                NewRoom.addItem(choice(FoodItems))
+            # Add a random number of random weapons
+            for _ in range(random.randint(0, 1)):
+                NewRoom.addItem(choice(WeaponItems))
+            
+            Row.append(NewRoom)
+        Rooms.append(Row)
+
+    currentRoom = Rooms[0][0]
     
     
     
-######################################################################
-# START THE GAME!!!
-inventory = [] # nothing in inventory...yet
+###################################################################### GAME
 createRooms() # add the rooms to the game
 # play forever (well, at least until the player dies or asks to quit)
 while (True):
 # set the status so the player has situational awareness
 # the status has room and inventory information
-    status = "{}\nYou are carrying: {}\n".format(currentRoom,inventory)
+    status = "{}\nYou are carrying: {}\n".format(currentRoom, ''.join([x.name + " " for x in Inventory]))
 # if the current room is None, then the player is dead
 # this only happens if the player goes south when in room 4
     if (currentRoom == None):
@@ -180,56 +238,37 @@ while (True):
         break
     # set a default response
     response = "I don't understand. Try verb noun. Valid verbs are go, look, and take"
-    # split the user input into words (words are separated by spaces)
     words = action.split()
-    # the game only understands two word inputs
     if (len(words) == 2):
     # isolate the verb and noun
         verb = words[0]
         noun = words[1]
-    # the verb is: go
-        if (verb == "go"):
-    # set a default response
-            response = "Invalid exit."
-    # check for valid exits in the current room
-            for i in range(len(currentRoom.exits)):
-    # a valid exit is found
-                if (noun == currentRoom.exits[i]):
-    # change the current room to the one that is
-    # associated with the specified exit
-                    currentRoom = currentRoom.exitLocations[i]
-    # set the response (success)
-                    response = "Room changed."
-    # no need to check any more exits
-                    break
-    # the verb is: look
-        elif (verb == "look"):
-# set a default response
+        if (verb == "look"):
             response = "I don't see that item."
-# check for valid items in the current room
             for i in range(len(currentRoom.items)):
-# a valid item is found
-                if (noun == currentRoom.items[i]):
-# set the response to the item's description
-                    response = currentRoom.itemDescriptions[i]
-# no need to check any more items
+                if (noun == currentRoom.items[i].name):
+                    response = currentRoom.items[i].description
+                    response += " It's grabbable." if item.grabbable else " It's not grabbable."
                     break
-    # the verb is: take
         elif (verb == "take"):
-    # set a default response
             response = "I don't see that item."
-    # check for valid grabbable items in the current room
-            for grabbable in currentRoom.grabbables:
-    # a valid grabbable item is found
-                if (noun == grabbable):
-    # add the grabbable item to the player's
-    # inventory
-                    inventory.append(grabbable)
-    # remove the grabbable item from the room
-                    currentRoom.delGrabbable(grabbable)
-    # set the response (success)
+            for item in currentRoom.items:
+                if noun == item.name and item.grabbable:
+                    Inventory.append(item)
+                    currentRoom.delItem(item)
                     response = "Item grabbed."
-    # no need to check any more grabbable items
                     break
-    # display the response
+                elif not item.grabbable:
+                    response = "That item isn't grabbable."
+                    break
+        elif verb == "eat":
+            response = "I don't have that item."
+            for item in Inventory:
+                if noun == item.name and item.__name__ == "Food":
+                    item.Use()
+                    response = "Food eaten."
+                    break
+                elif not item.__name__ == "Food":
+                    response = "I can't eat that!"
+                    break
     print("\n{}".format(response))
